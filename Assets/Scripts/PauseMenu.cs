0e3a5d0e97da91870acd7312b658c838a4d6fb3e
miquel02 <mgalmespascual@gmail.com>
2022-11-27 22:37:13 +0100

    27/11/22 22:37

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pausePanel;
+
+    public bool isPaused;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        pausePanel.SetActive(false);
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PauseGame()
+    {
+        if(isPaused == false)
+        {
+            pausePanel.SetActive(true);
+            Time.timeScale = 0;
+            isPaused = true;
+        }
+
+        if (isPaused == true)
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1;
+            isPaused = false;
+        }
+
+    }
+
+}
